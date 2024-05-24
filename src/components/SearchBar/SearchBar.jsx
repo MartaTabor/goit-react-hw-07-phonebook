afcd17c8a8301e860d1f5cd6adf37a13bb44f703
Marta Tabor <leeloothefifth@googlemail.com>
2024-05-24 20:50:41 +0100

    feat init

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import css from './SearchBar.module.css';
+
+export const SearchBar = ({ filter, onFilterChange }) => {
+  return (
+    <div className={css.search}>
+      <label className={css.label} htmlFor="searchField">
+        Find contacts by name
+      </label>
+      <input
+        className={css.input}
+        id="searchField"
+        type="text"
+        name="filter"
+        value={filter}
+        onChange={onFilterChange}
+        placeholder="Search by name"
+      />
+    </div>
+  );
+};
+
+SearchBar.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onFilterChange: PropTypes.func.isRequired,
+};
